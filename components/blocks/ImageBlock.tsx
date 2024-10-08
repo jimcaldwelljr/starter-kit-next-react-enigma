262878bc0fb2abff08142a5b7a8bcdc2731d1758
Hollie Duncan <hollie@contento.io>
2024-10-08 15:23:50 +0100

    Adds mobile image option to image block

--- components/blocks/ImageBlock.tsx
+++ components/blocks/ImageBlock.tsx
@@ -2,15 +2,36 @@ import { BlockData } from '@gocontento/client'
 import Image from 'next/image'
 
 export default function ImageBlock({ block }: { block: BlockData }) {
-  return block.fields.image.assets.length > 0 ? (
-    <div className="mx-auto max-w-5xl py-9">
-      <Image
-        src={block.fields.image.assets[0].asset.url}
-        alt={block.fields.image.assets[0].asset.description}
-        className="h-full w-full object-cover"
-        width={1000}
-        height={1000}
-      />
-    </div>
-  ) : null
+  return (
+    <>
+      {block.fields.mob_image.assets ? (
+        <div className="mx-auto max-w-5xl py-9">
+          <Image
+            src={block.fields.mob_image.assets[0].asset.url}
+            alt={block.fields.mob_image.assets[0].asset.description}
+            className="h-full w-full object-cover md:hidden"
+            width={600}
+            height={600}
+          />
+          <Image
+            src={block.fields.image.assets[0].asset.url}
+            alt={block.fields.image.assets[0].asset.description}
+            className="hidden h-full w-full object-cover md:block"
+            width={1000}
+            height={1000}
+          />
+        </div>
+      ) : (
+        <div className="mx-auto max-w-5xl py-9">
+          <Image
+            src={block.fields.image.assets[0].asset.url}
+            alt={block.fields.image.assets[0].asset.description}
+            className="hidden h-full w-full object-cover md:block"
+            width={1000}
+            height={1000}
+          />
+        </div>
+      )}
+    </>
+  )
 }
