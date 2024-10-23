eb16afd7fbc9f26e54bcbf0af2bbf0d3b450f406
Hollie Duncan <hollie@contento.io>
2024-10-23 11:48:28 +0100

    Updates mainNav and FooterNav validation in Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,23 @@ export default async function RootLayout({
   const mainNavId = process.env.SITE_MAIN_NAV_ID ?? false
   const footerNavId = process.env.SITE_FOOTER_NAV_ID ?? false
 
+  if (!mainNavId) {
+    throw new Error(
+      'No mainNav ID found - SITE_MAIN_NAV_ID - Please ensure you have created one in Contento and copied the ID to your .env file.',
+    )
+  }
+
+  if (!footerNavId) {
+    throw new Error(
+      'No FooterNav ID found - SITE_FOOTER_NAV_ID - Please ensure you have created one in Contento and copied the ID to your .env file.',
+    )
+  }
+
   const mainNav = await createClient()
     .getContentById(mainNavId)
     .catch(() => {
       throw new Error(
-        'No main nav found. Please ensure you have created one in Contento and copied the ID to your .env file.',
+        'No main nav found in Contento that matches the SITE_MAIN_NAV_ID',
       )
     })
 
@@ -38,7 +50,7 @@ export default async function RootLayout({
     .getContentById(footerNavId)
     .catch(() => {
       throw new Error(
-        'No footer nav found. Please ensure you have created one in Contento and copied the ID to your .env file.',
+        'No footer nav found in Contento that matches the SITE_FOOTER_NAV_ID',
       )
     })
 
